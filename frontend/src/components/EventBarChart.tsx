d1cd541fa92f2e48a9402eb7a84c2d1f0a52df57
uysalserkan <uysalserkan08@gmail.com>
2021-08-04 17:09:01 +0300

    closes #32 #23
    Admin sayfasındaki BarChart hatası düzeltildi. #22

--- frontend/src/components/EventBarChart.tsx
+++ frontend/src/components/EventBarChart.tsx
@@ -2,19 +2,17 @@ import {Bar, BarChart, Tooltip, XAxis, YAxis} from "recharts";
 import React from "react";
 
 function EventBarChart(props) {
-    console.log("props.eventData")
-    console.log(props.eventData)
+    // console.log("EVENTBARCHART A GELEN PROPS EVENTDATA")
+    // console.log(props.eventData)
 
     return (
         <BarChart width={1800} height={500}
-                  data={props.eventData
-                      .filter((each) => each.userCounter > 0)
-                      // .slice(0, props.eventData.length)
+                  data={props.eventData.slice(0, props.eventData.length)
                   }>
-            <XAxis dataKey={props.XKey} stroke="#8884d8"/>
+            <XAxis dataKey={props.XKey} stroke="#497382"/>
             <YAxis/>
-            <Tooltip/>
-            <Bar dataKey={props.barKey} fill="#8884d8" barSize={30}/>
+            <Tooltip label={props.XKey}/>
+            <Bar dataKey={props.barKey} fill="#497382" barSize={30}/>
         </BarChart>
     );
 }
